b304f9c55ae7649978f738f410514029870f8933
thaotrong2000 <trongthaouet@gmail.com>
2021-05-05 03:57:38 +0700

    3:57 day: 5/5/2021

--- MISA.Infrastructure/Repository/CustomerGroupRepository.cs
+++ MISA.Infrastructure/Repository/CustomerGroupRepository.cs
@@ -3,6 +3,11 @@ using MISA.Core.Interfaces.Repository;
 
 namespace MISA.Infrastructure.Repository
 {
+    /// <summary>
+    /// Xử lý dữ liệu cho CustomerGroup
+    /// Các hàm xử lý chung được kết thừa thông qua lớp BaseRepository
+    /// Custom thêm các hàm riêng của CustomerGroup
+    /// </summary>
     public class CustomerGroupRepository : BaseRepository<CustomerGroup>, ICustomerGroupRepository
     {
     }
--- MISA.Infrastructure/Repository/CustomerRepository.cs
+++ MISA.Infrastructure/Repository/CustomerRepository.cs
@@ -9,10 +9,20 @@ using System.Data;
 
 namespace MISA.Infrastructure.Repository
 {
+    /// <summary>
+    /// Tạo xử lý dữ liệu của riêng Customer
+    /// Kế thừa toàn bộ các hàm xử lý chung của BaseRepository
+    /// </summary>
     public class CustomerRepository : BaseRepository<Customer>, ICustomerRepository
     {
-        // Các hàm xử lý DATABASE
-
+        /// <summary>
+        /// Kiểm tra code đã tồn tại hay chưa?
+        /// </summary>
+        /// <param name="customerCode"></param>
+        /// <returns>
+        /// - true: CustomerCode đã tồn tại ( Không thể thêm )
+        /// - false: CustomerCode chưa tồn tại ( Có thể thêm )
+        /// </returns>
         public bool CheckCustomerCodeExist(string customerCode)
         {
             using (dbConnection = new MySqlConnection(connectString))
@@ -24,11 +34,29 @@ namespace MISA.Infrastructure.Repository
             }
         }
 
+        /// <summary>
+        /// Kiểm tra số điện thoại đã tồn tại chưa?
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns>
+        /// - true: SĐT đã tồn tại (Không thể thêm)
+        /// - false: SĐT chưa tồn tại (Có thể thêm)
+        /// </returns>
         public bool CheckPhoneNumberExist(string phoneNumber)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Thực hiện lọc dữ liệu theo số dữ liệu cần thiết, theo FullName, theo SĐT
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="fullName"></param>
+        /// <param name="phoneNumber"></param>
+        /// <returns>
+        /// customerFilter: Danh sách các Customer
+        /// </returns>
         public IEnumerable<Customer> CustomerFilter(int pageIndex, int pageSize, string fullName, string phoneNumber)
         {
             using (dbConnection = new MySqlConnection(connectString))
